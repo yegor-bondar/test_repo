b1f782178ba20f764bfee21c145aadeb60527bed
eg-bond <eg-bond@yandex.ru>
2024-12-18 18:32:35 +0300

    feat: func added

--- third.js
+++ third.js
@@ -1,1 +1,14 @@
 console.log('third feat 1');
+
+function thirdFeatBig() {
+  console.log('third feat 1');
+  console.log('third feat 2');
+  console.log('third feat 3');
+  console.log('third feat 4');
+  console.log('third feat 5');
+  console.log('third feat 6');
+  console.log('third feat 7');
+  console.log('third feat 8');
+  console.log('third feat 9');
+}
+
