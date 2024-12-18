5d3bf8eef922d0c082f829997e1a8b9dbba921f3
eg-bond <eg-bond@yandex.ru>
2024-12-18 18:59:28 +0300

    fix: fixed problem

--- third.js
+++ third.js
@@ -5,10 +5,6 @@ function thirdFeatBig() {
   console.log('third feat 2');
   console.log('third feat 3');
   console.log('third feat 4');
-  console.log('third feat 5');
-  console.log('third feat 6');
-  console.log('third feat 7');
-  console.log('third feat 8');
-  console.log('third feat 9');
+  console.log('third feat 5');  
 }
 
