4b951eca70fed485aa8c9b33fd837b36b8056f92
eg-bond <eg-bond@yandex.ru>
2024-12-18 17:36:10 +0300

    fix: fixes

--- another.js
+++ another.js
@@ -1,3 +1,15 @@
 console.log('another feat part 1');
-console.log('another feat part 2;;');
-console.log('unnecessary feat part 3');
+console.log('another feat part 2');
+
+function largeFunction() {
+  console.log('1');
+  console.log('2');
+  console.log('3');
+  console.log('4');
+  console.log('5');
+  console.log('6');
+  console.log('7');
+  console.log('8');
+  console.log('9');
+}
+
